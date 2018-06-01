74de37ec8408bc8536a3215db135d7666122de8e
Pedrojhms <j.pedrohenrique2011@gmail.com>
2018-06-01 17:19:49 -0300

    Corrigido a geração das entradas

--- Gerador-VUA/index.py
+++ Gerador-VUA/index.py
@@ -78,19 +78,6 @@ for x in range(1, 513):
             a3 = 1
         elif testa(i + 128):
             a3 = 0
-
-###############################     /GERACAO DAS ENTRADAS 'P' E 'G'/      ######################################
-    #CALCULO G DA VUA
-    G0 = a0 and b0
-    G1 = a1 and b1
-    G2 = a2 and b2
-    G3 = a3 and b3
-
-    #CALCULO P DA VUA
-    P0 = a0 or b0
-    P1 = a1 or b1
-    P2 = a2 or b2
-    P3 = a3 or b3
         
 ###############################     /GERACAO DAS ENTRADAS E SAIDAS DA VUA/      #################################
     #VERIFICO OS 256 CASOS COM O CIN EM '0', DEPOIS ALTERO PARA VERIFICACAO COM ELE EM '1'
@@ -100,20 +87,19 @@ for x in range(1, 513):
         C0 = 1
 
     #CALCULO DO VAI UM ANTECIPADO DA VUA
-    C1 = G0 or P0 and C0
-    C2 = G1 or P1 and C1
-    C3 = G2 or P2 and C2
-    C4 = G3 or P3 and C3
+    C1 = b0 or (a0 and C0)
+    C2 = b1 or (a1 and C1)
+    C3 = b2 or (a2 and C2)
     
     #SAIDA P DE GRUPO E G DE GRUPO
-    PG = P0 and P1 and P2 and P3
-    GG = G3 or G2 and P3 or G1 and P3 and P2 or G0 and P3 and P2 and P1
+    PG = a0 and a1 and a2 and a3
+    GG = b3 or b2 and a3 or b1 and a3 and a2 or b0 and a3 and a2 and a1
     
 ###############################     /IMPRIME OS VETORES DE TESTE/      ##########################################
     
-    print ("{}{}{}{}_{}{}{}{}_{}_{}_{}_{}{}{}{}".format(P3, P2, P1, P0, G3, G2, G1, G0, C0, GG, PG, C4, C3, C2, C1))
+    print ("{}{}{}{}_{}{}{}{}_{}_{}_{}_{}{}{}".format(a3, a2, a1, a0, b3, b2, b1, b0, C0, GG, PG, C3, C2, C1))
 
     #ALTERA O ARQUIVO DOS VETORES DE TESTE A CADA LINHA
     arquivo = open('vuafatorada.tv','a')
-    arquivo.write("{}{}{}{}_{}{}{}{}_{}_{}_{}_{}{}{}{}\n".format(P3, P2, P1, P0, G3, G2, G1, G0, C0, GG, PG, C4, C3, C2, C1))
+    arquivo.write("{}{}{}{}_{}{}{}{}_{}_{}_{}_{}{}{}\n".format(a3, a2, a1, a0, b3, b2, b1, b0, C0, GG, PG, C3, C2, C1))
     arquivo.close()
